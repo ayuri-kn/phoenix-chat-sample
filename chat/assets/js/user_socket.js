84be55882d7baa5f980af772c6a624b48a6a0128
ayuri-kn <yuri_atsushi@cyberagent.co.jp>
2022-12-08 16:45:11 +0900

    feat: add client side code

--- chat/assets/js/user_socket.js
+++ chat/assets/js/user_socket.js
@@ -2,11 +2,11 @@
 // you uncomment its entry in "assets/js/app.js".
 
 // Bring in Phoenix channels client library:
-import {Socket} from "phoenix"
+import { Socket } from "phoenix"
 
 // And connect to the path in "lib/chat_web/endpoint.ex". We pass the
 // token for authentication. Read below how it should be used.
-let socket = new Socket("/socket", {params: {token: window.userToken}})
+let socket = new Socket("/socket", { params: { token: window.userToken } })
 
 // When you connect, you'll often need to authenticate the client.
 // For example, imagine you have an authentication plug, `MyAuth`,
@@ -56,7 +56,26 @@ socket.connect()
 // Now that you are connected, you can join channels with a topic.
 // Let's assume you have a channel with a topic named `room` and the
 // subtopic is its id - in this case 42:
-let channel = socket.channel("room:42", {})
+// let channel = socket.channel("room:42", {})
+
+let channel = socket.channel("room:lobby", {})
+let chatInput = document.querySelector("#chat-input")
+let messagesContainer = document.querySelector("#messages")
+
+chatInput.addEventListener("keypress", event => {
+  if (event.key === 'Enter') {
+    channel.push("new_msg", { body: chatInput.value })
+    chatInput.value = ""
+  }
+})
+
+// display new message
+channel.on('new_msg', (payload) => {
+  const msgItem = document.createElement('li');
+  msgItem.innerText = `[${Date()}] ${payload.body}`;
+  messagesContainer.appendChild(msgItem);
+});
+
 channel.join()
   .receive("ok", resp => { console.log("Joined successfully", resp) })
   .receive("error", resp => { console.log("Unable to join", resp) })
